fe543d983a51c508249d259415488deccdfb886d
Ulyana <uliana.malakhova@yandex.ru>
2022-11-11 01:57:05 +0700

    add "general_data.cpp"

---
+++ 322/General_data.cpp
@@ -0,0 +1,35 @@
+#include "General_data.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+General_data::General_data(string country, string model, int year) {
+	this->country = country;
+	this->model = model;
+	this->year = year;
+}
+General_data::General_data(string model) {
+	this->model = model;
+}
+General_data::General_data() {
+
+}
+General_data::~General_data() {
+
+}
+void General_data::read() {
+	cout << "Введем информацию об общих данных" << endl;
+	cout << "Страна-производитель: ";
+	cin >> country;
+	cout << "Модель: ";
+	cin >> model;
+	cout << "Год выпуска: ";
+	cin >> year;
+}
+void General_data::display() {
+	cout << endl;
+	cout << "Общие данные:" << endl;
+	cout << "-страна-производитель:" << country << endl;
+	cout << "-модель:" << model << endl;
+	cout << "-год выпуска:" << year << endl;
+}
